f0731e14117c3e9ebf989faf4ad62f26e7108978
aleks20019010-web <aleks20019010@gmail.com>
2026-07-14 12:21:02 +0300

    chore: bump version to 2.3.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,7 +16,7 @@ android {
     minSdk = 24
     targetSdk = 36
     versionCode = 5
-    versionName = "2.3.1"
+    versionName = "2.3.2"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
